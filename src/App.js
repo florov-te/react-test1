b7364a5caff75a25885906e700029af02585cb06
tima <florovtf@gmail.com>
2019-12-24 12:26:39 +0300

    save

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+var image = React.createElement('img',{src:'https://facebook.github.io/react/img/logo.svg'});
+var element = React.createElement('h1',null,'привет react')
+var subtitle=React.createElement('p',null,',b,kbjntrf lkz cjplfybz gjkmpjdfntkmcrb[ bynthatqcjd')
+var container=React.createElement('div',{className:'container'},image,title,subtitle)
+ReactDOM.render(element,document.getElementById('root'))
+
+
+
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}*/
+
+//export default App;
